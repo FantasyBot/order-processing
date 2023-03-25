1fb9cad758b00ed1dd6bc68912f4c151d8b16bd6
FantasyBot <usernamegithub@gmail.com>
2023-03-26 01:46:03 +0400

    Add Global Secondary Indexes for DynamoDB

--- lambda_js/functions/check_inventory/function.ts
+++ lambda_js/functions/check_inventory/function.ts
@@ -1,30 +1,26 @@
-import { DynamoDB } from "aws-sdk";
-const docClient = new DynamoDB.DocumentClient();
+import AWS from "aws-sdk";
+const docClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10",
+  region: "eu-central-1",
+});
 
 export async function handler(input: any) {
-  console.log("input - - - >", input);
-
   const params = {
     TableName: "order-processing-dev-products_db",
+    IndexName: "product_name-index",
+    KeyConditionExpression: "product_name = :name",
+    FilterExpression: "count_in_stock <= :quantity",
+    ExpressionAttributeValues: {
+      ":name": "ram_240_gtx83-5",
+      ":quantity": 2,
+    },
   };
 
-  try {
-    // const data = await docClient.query(params).promise();
-    const data = await docClient.scan(params).promise();
-
-    console.log(" data -  -- - - - - > ", data);
-
-    if (data.Item) {
-      console.log("Product with name x exists in the table  -- - ", data);
-      // your processing logic here
-    } else {
-      console.log("Product with name x does not exist in the table");
-      // your processing logic here
-    }
-  } catch (err) {
-    console.log("Error: ", err);
-    // your error handling logic here?
-  }
+  const data = await docClient.query(params).promise();
+  // if (!data?.Item) {
+  //   throw new Error("ProductNotProvidedExeption");
+  // }
+  console.log("data - -  -> ", data);
 
   return input;
 }
