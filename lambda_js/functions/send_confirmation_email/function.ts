f28251408b377e9cdfcbe43a85da88c313d4e65b
FantasyBot <usernamegithub@gmail.com>
2023-04-06 00:28:36 +0400

    add AWS SES functionality

--- lambda_js/functions/send_confirmation_email/function.ts
+++ lambda_js/functions/send_confirmation_email/function.ts
@@ -1,33 +1,37 @@
-import { createEmailTransport, sendEmail } from "../../utils/email.util";
+import AWS from "aws-sdk";
 
 export async function handler(input: any) {
-  console.log("event in send_email  ---> ", input);
+  const ses = new AWS.SES();
 
-  // console.log("from email  - - - -  > ", process.env.G_APP_FROM_EMAIL);
-
-  // const transport = await createEmailTransport(
-  //   process.env.G_APP_FROM_EMAIL as string,
-  //   process.env.G_APP_CLIENT_ID as string,
-  //   process.env.G_APP_CLIENT_SECRET as string,
-  //   process.env.G_APP_REFRESH_TOKEN as string
-  // );
-
-  // const data = await sendEmail(transport, {
-  //   to: input.to,
-  //   subject: input.subject,
-  //   html: input.content,
-  //   cc: input.cc,
-  //   bcc: input.bcc,
-  //   attachments: input.attachments,
-  // });
-
-  // transport.close();
-
-  // console.log("email data -  - - >", data);
+  const params = {
+    Destination: {
+      ToAddresses: [input.costumer.email],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: `<div>
+                   <h2>Your order (${input.product.name}) has been shipped!</h2>
+                   <h2>Product Information:</h2>
+                   <p>product name : ${input.shipping_information.product_name}</p>
+                   <p>quantity : ${input.product.quantity}</p>
+                   <p>product price : ${input.shipping_information.product_price}</p>
+                   <p>address : ${input.costumer.address}</p>
+                 </div>`,
+        },
+      },
+      Subject: {
+        Data: "Order Confirmation",
+      },
+    },
+    Source: process.env.FROM_EMAIL as string,
+  };
+  await ses.sendEmail(params).promise();
 
   return input;
 }
 
-// I could use `AWS SES` but in free tier it has only 200 email in a day for free (6200 in month).
-// The main reason to swith google email service was that `AWS SES` without encreasing limits had to verify every email - `sender` and `receiver`
-// If you encrease the limit (not free tier as I know) you would be able to send 50000 email each month and you have to verify only `sender` email.
+// `AWS SES` in free tier has only 200 email in a day for free (6200 in month).
+//  without encreasing limits had to verify every email - `sender` and `receiver`
+//  If you encrease the limit (not free tier as I know) you would be able to send 50000 email each month
+//  and you have to verify only `sender` email.
